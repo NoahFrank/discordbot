4f37859c3dac7f098f8bece64c9095844d4e8b9e
Noah Frank <udadaadaa@gmail.com>
2017-01-24 15:33:23 -0500

    - Initial Commit using yo and generator-discordbot

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+var jeeves = require('../lib');
+
+describe('jeeves', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
